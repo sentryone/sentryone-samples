44bbe7a6511663c25667b7854bc773a7c5abc770
Melissa Connors <40669268+MelissaConnors@users.noreply.github.com>
2021-03-01 09:08:54 -0500

    Create Watch all targets listed in text file.ps1

---
+++ powershell/Watch all targets listed in text file.ps1
@@ -0,0 +1,11 @@
+<#Change this to match the name of the server housing the SentryOne database and the name of the SentryOne database #>
+Connect-SQLSentry -ServerName servername -DatabaseName SentryOne
+
+<#Change this to match the name of the text file that includes all the names of all the servers you want to watch #>
+$Servers = Get-Content C:\Powershell\TestWatch.txt
+
+Foreach ($Server in $Servers)
+{
+    Register-Connection -ConnectionType SqlServer -Name $Server
+    Get-Connection -Name $Server -NamedServerConnectionType SqlServer| Invoke-WatchConnection
+}
