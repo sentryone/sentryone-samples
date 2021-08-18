98026fea2a4d18d4ca21a2f474a5fa5a76a7b8da
Stephen Dyckes <55807591+SDyckes@users.noreply.github.com>
2021-08-18 17:15:44 -0400

    Add quotes around file path
    I added double quotes around the file path to accommodate a file path with spaces

--- powershell/Watch all targets listed in text file.ps1
+++ powershell/Watch all targets listed in text file.ps1
@@ -2,7 +2,7 @@
 Connect-SQLSentry -ServerName servername -DatabaseName SentryOne
 
 <#Change this to match the name of the text file that includes all the names of all the servers you want to watch #>
-$Servers = Get-Content C:\Powershell\TestWatch.txt
+$Servers = Get-Content "C:\Powershell\TestWatch.txt"
 
 Foreach ($Server in $Servers)
 {
